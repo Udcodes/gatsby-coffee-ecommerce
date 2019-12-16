9103bfbba57fca37ef51af7df08479072bd31051
Udcodes <blessingudochi@gmail.com>
2019-12-16 11:16:09 +0100

    adjusted conifig

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,8 +29,10 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: `${process.env.CONTENTFUL_ID}`,
+        // process.env.CONTENTFUL_SPACE_ID,
+        accessToken: `${process.env.CONTENTFUL_TOKEN}`,
+        // process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
     {
